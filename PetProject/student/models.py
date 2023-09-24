6b0da2d25a0423e50edd750060987873c061265c
nazartivanyuk <nazar.ivanyuk@gmail.com>
2023-09-24 16:43:34 -0700

    Create student model

--- PetProject/student/models.py
+++ PetProject/student/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
-
-# Create your models here.
+class Student(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.IntegerField(default=1)
+    phone_number = models.CharField(max_length=12, default=None)
+    def __str__(self):
+        return self.user.username
